c52923130003c983393f28770d1c66a490a7dcae
김성진(XW - KIM SUNG JIN) <kim.sungj@koreanair.com>
2017-12-05 14:57:28 +0900

    heroku를 위해 listen 포트 추가

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use("/cardissuers", require("./routes/cardissuers"));
 
 
 // Port setting
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("server on!");
 });
