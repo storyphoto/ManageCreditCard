18f22e4667a30726b7ad209b804c1ca0a64cb29c
storyphoto <storyphoto@gmail.com>
2017-12-08 17:47:13 +0900

    kakao oauth node js에 개발

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router  = express.Router();
-
+var request = require('request');
  
 // Index
 router.get("/", function(req, res){
@@ -13,7 +13,28 @@ router.get("/", function(req, res){
 // kakao_oauth
 router.get("/kakao_oauth", function(req, res){
   
-      res.redirect("/CreditCardIssuerList");
+    var client_id = '30e0b78f1a3746f3f1383f4a972d6331';
+    var redirect_url = '/kakao_oauth';
+
+    var url = 'https://kapi.kakao.com/v1/user/me';
+    
+    var oRequestParam = {
+        method: 'POST',
+        url: url,
+        headers : {"Authorization" : "Bearer V9ICniqd93PWrEcJ-MWoeWrQB6pf-mOksXliLwopdkgAAAFgNTVCoA"}
+    };
+
+    var sendData = '';
+    request(oRequestParam, function (error, response, body) {
+  
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+               res.send(body);
+               res.render('oauth', {body: body});
+        } else {
+            console.log('error');             
+        };
+    })
    
   });
  
